05228d7a601c27cab198e21b7f3623852a9336cb
Sergey Pribytkov <s.pribytkov@argustelecom.ru>
2021-02-06 20:20:33 +0700

    ERTHSobes, start

---
+++ ERTHSobes/Controllers/СreateObjectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERTHSobes.Controllers
+{   
+    [ApiController]
+    [Route("api/createobject")]
+    public class СreateObjectController : ControllerBase
+    {
+        [HttpGet]
+        public Guid Get()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
